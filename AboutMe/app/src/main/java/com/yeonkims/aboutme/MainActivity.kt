212ba2329b03cc22fb0e3b7eb7dbc76ad9084251
yeonkims <jiyeon7269@naver.com>
2022-05-19 15:16:01 +0100

    setContentView 주석 제거

--- AboutMe/app/src/main/java/com/yeonkims/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/yeonkims/aboutme/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // We can now access the doneButton through to binding object.
