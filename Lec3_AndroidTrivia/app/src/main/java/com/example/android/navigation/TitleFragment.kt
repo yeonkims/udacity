c8d92205240360b5e7386990d7386e5fb40e848f
yeonkims <jiyeon7269@naver.com>
2022-05-20 12:57:49 +0100

    connecting to another framgent

--- Lec3_AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Lec3_AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_title2, container, false)
 
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
 
         return binding.root
     }
