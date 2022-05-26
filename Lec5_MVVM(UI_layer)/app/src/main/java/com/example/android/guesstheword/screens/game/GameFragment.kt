eacb2e929e688d2f6cab63b8a1a1bc172812a976
yeonkims <jiyeon7269@naver.com>
2022-05-26 22:24:42 +0100

    ViewModel to DataBinding (OnClick)

--- Lec5_MVVM(UI_layer)/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Lec5_MVVM(UI_layer)/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -52,15 +52,9 @@ class GameFragment : Fragment() {
                 false
         )
 
-        Log.i("GameFragment","Called ViewModelProviders.of!")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
+        binding.gameViewModel = viewModel
 
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
--- Lec5_MVVM(UI_layer)/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ Lec5_MVVM(UI_layer)/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -58,12 +58,12 @@ class ScoreFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
 
+        binding.scoreViewModel = viewModel
+
         // Add observer for score
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
         })
-
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         // Navigates back to title when button is pressed
         viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
