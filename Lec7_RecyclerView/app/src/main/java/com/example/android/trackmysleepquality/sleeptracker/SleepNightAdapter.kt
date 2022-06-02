79f7d9a9e13f676a38d760865562e53906fed05a
yeonkims <jiyeon7269@naver.com>
2022-06-02 15:57:43 +0100

    Initial commit

---
+++ Lec7_RecyclerView/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+// TODO (02) Create SleepNightAdapter class and extend it
+// from RecyclerView.Adapter<TextItemViewHolder>
+
+// TODO (03) Create a variable, data, that holds a list of SleepNight.
+
+// TODO (04) Override getItemCount() to return the total number of items in the data set.
+
+// TODO (05) Override onBindViewHolder() and have it update the contents of the
+// ViewHolder to reflect the item at the given position.
+
+// TODO (06) Override onCreateViewHolder(). We'll complete this method
+// in a later exercise.
